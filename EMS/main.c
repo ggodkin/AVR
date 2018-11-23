21808f0b3c872fa55bd47dd135ec3a03681124b1
Greg Godkin <grgodkin@yahoo.com>
2018-11-23 08:54:31 -0600

    CTC mode. Cannot get OC0A and OC0B to be in different phases

--- EMS/main.c
+++ EMS/main.c
@@ -8,23 +8,24 @@
 // Interrupt Routines
 // ********************************************************************************
 // timer1 overflow
-ISR(TIM1_COMPA_vect) {
-    PORTA^= (1<<PA2);
-}
+//ISR(TIM1_COMPA_vect) {
+//    PORTA^= (1<<PA2);
+//}
 
-ISR(TIM0_COMPA_vect) {
-    PORTA^= ((1<<PA7) | (1<<PA3));
-}
+//ISR(TIM0_COMPA_vect) {
+//    PORTA^= ((1<<PA7) | (1<<PA3));
+//}
 
 //not used
-ISR(TIM0_COMPB_vect) {
-    PORTA^= (1<<PA1);
-}
+//ISR(TIM0_COMPB_vect) {
+//    PORTA^= (1<<PA1);
+//}
 
 void init_ports(){
     // Configure PORTA as output
     DDRA = 0xFF;
-    PORTA = ((1<<PA7)) ;
+    PORTA = 0x00;
+    //PORTA = ((1<<PA7)) ;
     DDRB = 0xF;
     PORTB = (1<<PB2);
 }
@@ -36,18 +37,22 @@ void init_cpu(){
     //CLKPR = ((1<<CLKPS0)); //CPS scaler 2
 }
 void init_timers(){
-    //set CTC mode
-    TCCR0A |= ((1<<WGM01) | (1<<COM0A1) ); //((1<<WGM01) | (1<<COM0A0));   //1<<COM0A0 -- Toggle OC0A, do not need interrupt; 1<<WGM01 -- CTC mode
-    //set prescaler
+    int MyTimer = 58;
+    //Stop timer
+    TCCR0B = 0x00;
+    //output register
+    OCR0A |= MyTimer;
+    OCR0B |= MyTimer;
+    init_ports();
+     //set prescaler
     TCCR0B |= ((1<<CS00) | (1<<CS02));  //1024
     //TCCR0B |= ((1<<CS02));  //256
     //TCCR0B |= ((1<<CS00) | (1<<CS01));  //64
     //TCCR0B |= ((1<<CS01));  //8
     //TCCR0B |= ((1<<CS00));  //1
-    //output register
-    OCR0A |= 58;
-    //OCR0B |= 255; not used
-    TIMSK0 |= (1<<OCIE0A);
+    //set CTC mode
+    TCCR0A |= ((1<<WGM01) | (1<<COM0A0) | (1<<COM0B0 )); //((1<<WGM01) | (1<<COM0A0));   //1<<COM0A0 -- Toggle OC0A, do not need interrupt; 1<<WGM01 -- CTC mode
+   //TIMSK0 |= (1<<OCIE0A);
 }
 
 // ********************************************************************************
@@ -55,10 +60,9 @@ void init_timers(){
 // ********************************************************************************
 int main( void ) {
     init_cpu();
-    init_ports();
     init_timers();
     // enable timer overflow interrupt for both Timer0 and Timer1
-    sei();
+    //sei();
     while(1) {
     }
 }
