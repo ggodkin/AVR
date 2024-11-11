534084dfcd35a075ecb0c5c8420adc3bdd5022af
greg <grgodkin@yahoo.com>
2024-11-11 15:20:21 -0700

    working code without averaging

--- CapacitanceMeterOriginal/CapacitanceMeterOriginal.ino
+++ CapacitanceMeterOriginal/CapacitanceMeterOriginal.ino
@@ -1,47 +1,35 @@
-/* 
-Capacitance meter
-sourced from https://www.circuitbasics.com/how-to-make-an-arduino-capacitance-meter/#
-*/
-
 const int OUT_PIN = A2;
 const int IN_PIN = A0;
 const float IN_STRAY_CAP_TO_GND = 24.48;
 const float IN_CAP_TO_GND  = IN_STRAY_CAP_TO_GND;
 const float R_PULLUP = 34.8;  
 const int MAX_ADC_VALUE = 1023;
-const int NUM_SAMPLES = 64;  // Number of samples for averaging
 
-unsigned long previousMillis = 0;
-int sampleCount = 0;
-float totalCapacitance = 0;
-
-void setup() {
+void setup(){
   pinMode(OUT_PIN, OUTPUT);
   pinMode(IN_PIN, OUTPUT);
   Serial.begin(9600);
 }
 
-void loop() {
-  if (sampleCount < NUM_SAMPLES) {
-    // Introduce a delay between each measurement
-    delay(10);
-
+void loop(){
     pinMode(IN_PIN, INPUT);
     digitalWrite(OUT_PIN, HIGH);
     int val = analogRead(IN_PIN);
     digitalWrite(OUT_PIN, LOW);
 
-    float capacitance;
-    if (val < 1000) {
+    if (val < 1000){
       pinMode(IN_PIN, OUTPUT);
-      capacitance = (float)val * IN_CAP_TO_GND / (float)(MAX_ADC_VALUE - val);
+
+      float capacitance = (float)val * IN_CAP_TO_GND / (float)(MAX_ADC_VALUE - val);
 
       Serial.print(F("Capacitance Value = "));
       Serial.print(capacitance, 3);
       Serial.print(F(" pF ("));
       Serial.print(val);
       Serial.println(F(") "));
-    } else {
+    }
+
+    else{
       pinMode(IN_PIN, OUTPUT);
       delay(1);
       pinMode(OUT_PIN, INPUT_PULLUP);
@@ -49,13 +37,13 @@ void loop() {
       unsigned long t;
       int digVal;
 
-      do {
+      do{
         digVal = digitalRead(OUT_PIN);
         unsigned long u2 = micros();
         t = u2 > u1 ? u2 - u1 : u1 - u2;
       } while ((digVal < 1) && (t < 400000L));
 
-      pinMode(OUT_PIN, INPUT);
+      pinMode(OUT_PIN, INPUT);  
       val = analogRead(OUT_PIN);
       digitalWrite(IN_PIN, HIGH);
       int dischargeTime = (int)(t / 1000L) * 5;
@@ -64,13 +52,14 @@ void loop() {
       digitalWrite(OUT_PIN, LOW);
       digitalWrite(IN_PIN, LOW);
 
-      capacitance = -(float)t / R_PULLUP / log(1.0 - (float)val / (float)MAX_ADC_VALUE);
+      float capacitance = -(float)t / R_PULLUP / log(1.0 - (float)val / (float)MAX_ADC_VALUE);
 
       Serial.print(F("Capacitance Value = "));
-      if (capacitance > 1000.0) {
+      if (capacitance > 1000.0){
         Serial.print(capacitance / 1000.0, 2);
         Serial.print(F(" uF"));
-      } else {
+      }
+      else{
         Serial.print(capacitance, 2);
         Serial.print(F(" nF"));
       }
@@ -83,22 +72,5 @@ void loop() {
       Serial.print(val);
       Serial.println(F(")"));
     }
-
-    totalCapacitance += capacitance;
-    sampleCount++;
-  } else {
-    // Calculate the average capacitance
-    float averageCapacitance = totalCapacitance / (float)NUM_SAMPLES;
-
-    // Output capacitance for debugging purposes
-    Serial.print(F("Average Capacitance = "));
-    Serial.print(averageCapacitance, 3);
-    Serial.println(F(" pF"));
-
-    // Reset for the next cycle
-    sampleCount = 0;
-    totalCapacitance = 0;
-  }
-
-  while (millis() % 1000 != 0);    
+    while (millis() % 1000 != 0);    
 }
--- VaterLevelIndicator/VaterLevelIndicator.ino
+++ VaterLevelIndicator/VaterLevelIndicator.ino
@@ -6,114 +6,98 @@ sourced from https://www.circuitbasics.com/how-to-make-an-arduino-capacitance-me
 const int OUT_PIN = A2;
 const int IN_PIN = A0;
 const float IN_STRAY_CAP_TO_GND = 24.48;
-const float IN_CAP_TO_GND = IN_STRAY_CAP_TO_GND;
+const float IN_CAP_TO_GND  = IN_STRAY_CAP_TO_GND;
 const float R_PULLUP = 34.8;  
 const int MAX_ADC_VALUE = 1023;
-const int NUM_SAMPLES = 64;
-const unsigned long MEASUREMENT_INTERVAL = 10; // Time between measurements in milliseconds
 
 const int ledPins[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11}; // Pins connected to the LED bar graph
 const int NUM_LEDS = sizeof(ledPins) / sizeof(ledPins[0]);
 
-float minLevel = 100.0;  // Minimum capacitance level
-float maxLevel = 1000.0; // Maximum capacitance level
-
-unsigned long previousMillis = 0;
-int sampleCount = 0;
-long totalVal = 0;
-unsigned long totalTime = 0;
+float minLevel = 12.8;  // Minimum capacitance level based on measurements
+float maxLevel = 19.5;  // Maximum capacitance level based on measurements
 
 void setup() {
-  pinMode(OUT_PIN, OUTPUT);
-  pinMode(IN_PIN, OUTPUT);
-  Serial.begin(9600);
-  
-  // Set LED pins as outputs
-  for (int i = 0; i < NUM_LEDS; i++) {
-    pinMode(ledPins[i], OUTPUT);
-    digitalWrite(ledPins[i], LOW);
-  }
+    pinMode(OUT_PIN, OUTPUT);
+    pinMode(IN_PIN, OUTPUT);
+    Serial.begin(9600);
+
+    // Set LED pins as outputs
+    for (int i = 0; i < NUM_LEDS; i++) {
+        pinMode(ledPins[i], OUTPUT);
+        digitalWrite(ledPins[i], LOW);
+    }
 }
 
 void loop() {
-  unsigned long currentMillis = millis();
-
-  if (currentMillis - previousMillis >= MEASUREMENT_INTERVAL) {
-    previousMillis = currentMillis;
-
-    // Take a sample
     pinMode(IN_PIN, INPUT);
     digitalWrite(OUT_PIN, HIGH);
     int val = analogRead(IN_PIN);
     digitalWrite(OUT_PIN, LOW);
 
+    float capacitance;
     if (val < 1000) {
-      pinMode(IN_PIN, OUTPUT);
-      totalVal += val;
-    } else {
-      pinMode(IN_PIN, OUTPUT);
-      pinMode(OUT_PIN, INPUT_PULLUP);
-      unsigned long u1 = micros();
-      unsigned long t;
-      int digVal;
+        pinMode(IN_PIN, OUTPUT);
+        capacitance = (float)val * IN_CAP_TO_GND / (float)(MAX_ADC_VALUE - val);
 
-      do {
-        digVal = digitalRead(OUT_PIN);
-        unsigned long u2 = micros();
-        t = (u2 > u1) ? (u2 - u1) : (u1 - u2);
-      } while ((digVal < 1) && (t < 400000L));
-
-      pinMode(OUT_PIN, INPUT);  
-      val = analogRead(OUT_PIN);
-      digitalWrite(IN_PIN, HIGH);
-      totalTime += t * 2;  // Adjust timing for 8MHz clock
-    }
-
-    sampleCount++;
-
-    // If we've taken the required number of samples, calculate the average and capacitance
-    if (sampleCount >= NUM_SAMPLES) {
-      float averageVal = totalVal / (float)NUM_SAMPLES;
-      float averageTime = totalTime / (float)NUM_SAMPLES;
-
-      float capacitance;
-      if (averageVal < 1000) {
-        capacitance = averageVal * IN_CAP_TO_GND / (float)(MAX_ADC_VALUE - averageVal);
         Serial.print(F("Capacitance Value = "));
         Serial.print(capacitance, 3);
         Serial.print(F(" pF ("));
-        Serial.print(averageVal);
+        Serial.print(val);
         Serial.println(F(") "));
-      } else {
-        capacitance = -(float)averageTime / R_PULLUP / log(1.0 - (float)averageVal / (float)MAX_ADC_VALUE);
+    } else {
+        pinMode(IN_PIN, OUTPUT);
+        delay(1);
+        pinMode(OUT_PIN, INPUT_PULLUP);
+        unsigned long u1 = micros();
+        unsigned long t;
+        int digVal;
+
+        do {
+            digVal = digitalRead(OUT_PIN);
+            unsigned long u2 = micros();
+            t = u2 > u1 ? u2 - u1 : u1 - u2;
+        } while ((digVal < 1) && (t < 400000L));
+
+        pinMode(OUT_PIN, INPUT);
+        val = analogRead(OUT_PIN);
+        digitalWrite(IN_PIN, HIGH);
+        int dischargeTime = (int)(t / 1000L) * 5;
+        delay(dischargeTime);
+        pinMode(OUT_PIN, OUTPUT);
+        digitalWrite(OUT_PIN, LOW);
+        digitalWrite(IN_PIN, LOW);
+
+        capacitance = -(float)t / R_PULLUP / log(1.0 - (float)val / (float)MAX_ADC_VALUE);
+
         Serial.print(F("Capacitance Value = "));
         if (capacitance > 1000.0) {
-          Serial.print(capacitance / 1000.0, 2);
-          Serial.print(F(" uF"));
+            Serial.print(capacitance / 1000.0, 2);
+            Serial.print(F(" uF"));
         } else {
-          Serial.print(capacitance, 2);
-          Serial.print(F(" nF"));
+            Serial.print(capacitance, 2);
+            Serial.print(F(" nF"));
         }
-        Serial.print(F(" (t= "));
-        Serial.print(averageTime);
+
+        Serial.print(F(" ("));
+        Serial.print(digVal == 1 ? F("Normal") : F("HighVal"));
+        Serial.print(F(", t= "));
+        Serial.print(t);
         Serial.print(F(" us, ADC= "));
-        Serial.print(averageVal);
+        Serial.print(val);
         Serial.println(F(")"));
-      }
-
-      // Calculate the liquid level as a percentage
-      float level = map(capacitance, minLevel, maxLevel, 0, NUM_LEDS);
-      level = constrain(level, 0, NUM_LEDS);
-
-      // Update the LED bar graph
-      for (int i = 0; i < NUM_LEDS; i++) {
-        digitalWrite(ledPins[i], i < level ? HIGH : LOW);
-      }
-
-      // Reset for the next cycle
-      sampleCount = 0;
-      totalVal = 0;
-      totalTime = 0;
     }
-  }
+
+    // Manual level calculation for debugging
+    float level = (capacitance - minLevel) * NUM_LEDS / (maxLevel - minLevel);
+    level = constrain(level, 0, NUM_LEDS);
+
+    Serial.print(F("Manually Calculated Level = "));
+    Serial.println(level);
+
+    // Update the LED bar graph
+    for (int i = 0; i < NUM_LEDS; i++) {
+        digitalWrite(ledPins[i], i < level ? HIGH : LOW);
+    }
+
+    while (millis() % 1000 != 0);
 }
